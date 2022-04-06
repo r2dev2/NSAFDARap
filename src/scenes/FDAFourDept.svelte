aaf9c1cf38dc257ce2d717e6af4b7b5fcf6b761c
r2dev2 <ronak.badhe@gmail.com>
2022-04-05 23:50:02 -0700

    vfx: califf and his stache

--- src/scenes/FDAFourDept.svelte
+++ src/scenes/FDAFourDept.svelte
@@ -5,8 +5,51 @@
   const dispatch = createEventDispatcher();
 
   $: if ($time > 204.3) {
-    dispatch('ended');
+    // dispatch('ended');
   }
 </script>
 
 <video src="./sources/fdafourdept.mp4" autoplay muted />
+
+<img
+  class="califf"
+  class:show={186.1 < $time && $time < 190}
+  src="./sources/califf.jpg"
+/>
+
+{#if 187.7 < $time && $time < 190}
+  <img class="califf-stache" src="./sources/califfstache.png" />
+{/if}
+
+<style>
+  .califf, .califf-stache {
+    position: absolute;
+    top: 2rem;
+    right: 2rem;
+    width: 40%;
+  }
+
+  .califf {
+    transition: 900ms ease-out;
+    transform: scale(0);
+  }
+
+  .califf.show {
+    transform: scale(1);
+  }
+
+  .califf-stache {
+    transform-origin: 376px 224px;
+    animation: pulse 1.5s ease-out alternate infinite;
+  }
+
+  @keyframes pulse {
+    from {
+      transform: scale(0);
+    }
+
+    to {
+      transform: scale(5);
+    }
+  }
+</style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { stopwatch } from './common';
 
-export const startFrom = 200;
+export const startFrom = 180;
 export const rapAudio = new Audio('./sources/rap.mp3');
 export const time = stopwatch(startFrom);
