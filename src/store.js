d963c0dd8155e3fb3e0610ea71eaf04f4e78472e
r2dev2 <ronak.badhe@gmail.com>
2022-04-05 23:31:17 -0700

    fix: sync up ending properly

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { stopwatch } from './common';
 
-export const startFrom = 210;
+export const startFrom = 200;
 export const rapAudio = new Audio('./sources/rap.mp3');
 export const time = stopwatch(startFrom);
